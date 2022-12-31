1f17f2c6b0ec884826479e8fc5d68f3ecf97edd2
Ricardo Rosales <728243+missingcharacter@users.noreply.github.com>
2022-12-30 23:08:43 -0600

    Adding known domainds for ads, tracking, and/or malware

---
+++ scripts/update-hosts-file.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#!/data/data/com.termux/files/usr/bin/env bash
+# Enable bash's unofficial strict mode
+GITROOT=$(git rev-parse --show-toplevel)
+. ${GITROOT}/lib/strict-mode
+strictMode
+# Enabling utils
+. ${GITROOT}/lib/utils
+
+declare -a HOSTS_URLS=(
+  'https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts'
+  'https://adaway.org/hosts.txt'
+  'https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/android-tracking.txt'
+  'https://zerodot1.gitlab.io/CoinBlockerLists/hosts'
+  'https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-blocklist.txt'
+  'https://hblock.molinero.dev/hosts'
+  'https://www.netguard.me/hosts'
+)
+
+curl -sL "${HOSTS_URLS[@]}" | \
+  sed '/\(local\|^#\|^$\|^[[:space:]]*$\|^255.255.255.255\|^0.0.0.0 0.0.0.0\|^::1\|^ff0[02]:\)/d' | \
+  sed 's!^\(127\|0\)\.0\.0\.\(1\|0\) !!g' | \
+  sort -u | \
+  sed 's!^!0.0.0.0 !g'
